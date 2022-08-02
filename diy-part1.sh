50c617f80f2c8bd6f90fb892605331a5d9474365
Blue planet <30463977+Blueplanet20120@users.noreply.github.com>
2022-08-02 21:02:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,10 +12,10 @@
 
 # Uncomment a feed source
 # Add a feed source
-sed -i "/helloworld/d" "feeds.conf.default"
-echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
-echo 'src-git passwallluci https://github.com/xiaorouji/openwrt-passwall;luci' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall;packages' >>feeds.conf.default
+# sed -i "/helloworld/d" "feeds.conf.default"
+# echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+# echo 'src-git passwallluci https://github.com/xiaorouji/openwrt-passwall;luci' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall;packages' >>feeds.conf.default
 
 mkdir -p files/usr/share
 mkdir -p files/etc/
@@ -31,46 +31,29 @@ cat>rename.sh<<-\EOF
 #!/bin/bash
 rm -rf  bin/targets/x86/64/config.buildinfo
 rm -rf  bin/targets/x86/64/feeds.buildinfo
-rm -rf  bin/targets/x86/64/openwrt-x86-64-generic-kernel.bin
-rm -rf  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.vmdk
-rm -rf  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.vmdk
-rm -rf  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-rootfs.img.gz
-rm -rf  bin/targets/x86/64/openwrt-x86-64-generic.manifest
+rm -rf  bin/targets/x86/64/immortalwrt-x86-64-generic-kernel.bin
+rm -rf  bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-rootfs.img.gz
+rm -rf  bin/targets/x86/64/immortalwrt-x86-64-generic-rootfs.tar.gz
+rm -rf  bin/targets/x86/64/immortalwrt-x86-64-generic.manifest
 rm -rf bin/targets/x86/64/sha256sums
 rm -rf  bin/targets/x86/64/version.buildinfo
 sleep 2
 rename_version=`cat files/etc/lenyu_version`
 str1=`grep "KERNEL_PATCHVER:="  target/linux/x86/Makefile | cut -d = -f 2` #判断当前默认内核版本号如5.10
-ver414=`grep "LINUX_VERSION-4.14 ="  include/kernel-4.14 | cut -d . -f 3`
-ver419=`grep "LINUX_VERSION-4.19 ="  include/kernel-4.19 | cut -d . -f 3`
-ver54=`grep "LINUX_VERSION-5.4 ="  include/kernel-5.4 | cut -d . -f 3`
-ver510=`grep "LINUX_VERSION-5.10 ="  include/kernel-5.10 | cut -d . -f 3`
-ver515=`grep "LINUX_VERSION-5.15 ="  include/kernel-5.15 | cut -d . -f 3`
-if [ "$str1" = "5.4" ];then
-   mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/openwrt_x86-64-${rename_version}_${str1}.${ver54}_sta_Lenyu.img.gz
-  mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/openwrt_x86-64-${rename_version}_${str1}.${ver54}_uefi-gpt_sta_Lenyu.img.gz
-elif [ "$str1" = "4.19" ];then
-  mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/openwrt_x86-64-${rename_version}_${str1}.${ver419}_sta_Lenyu.img.gz
-  mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/openwrt_x86-64-${rename_version}_${str1}.${ver419}_uefi-gpt_sta_Lenyu.img.gz
-elif [ "$str1" = "4.14" ];then
-  mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/openwrt_x86-64-${rename_version}_${str1}.${ver414}_sta_Lenyu.img.gz
-  mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/openwrt_x86-64-${rename_version}_${str1}.${ver414}_uefi-gpt_sta_Lenyu.img.gz
-elif [ "$str1" = "5.10" ];then
-   mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/openwrt_x86-64-${rename_version}_${str1}.${ver510}_sta_Lenyu.img.gz
-  mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/openwrt_x86-64-${rename_version}_${str1}.${ver510}_uefi-gpt_sta_Lenyu.img.gz
-elif [ "$str1" = "5.15" ];then
-   mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/openwrt_x86-64-${rename_version}_${str1}.${ver515}_sta_Lenyu.img.gz
-  mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/openwrt_x86-64-${rename_version}_${str1}.${ver515}_uefi-gpt_sta_Lenyu.img.gz
-fi
+ver54=`grep "LINUX_VERSION-5.4 ="  include/kernel-version.mk | cut -d . -f 3`
+sleep 2
+mv  bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/immortalwrt_x86-64-${rename_version}_${str1}.${ver54}_sta_Lenyu.img.gz
+mv  bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/immortalwrt_x86-64-${rename_version}_${str1}.${ver54}_uefi-gpt_sta_Lenyu.img.gz
+sleep 2
 ls bin/targets/x86/64 | grep "_Lenyu.img" | cut -d - -f 3 | cut -d _ -f 1-2 > wget/op_version1
 #md5
 ls -l  "bin/targets/x86/64" | awk -F " " '{print $9}' > wget/open_sta_md5
 sta_version=`grep "_uefi-gpt_sta_Lenyu.img.gz" wget/open_sta_md5 | cut -d - -f 3 | cut -d _ -f 1-2`
-openwrt_sta=openwrt_x86-64-${sta_version}_sta_Lenyu.img.gz
-openwrt_sta_uefi=openwrt_x86-64-${sta_version}_uefi-gpt_sta_Lenyu.img.gz
+immortalwrt_sta=immortalwrt_x86-64-${sta_version}_sta_Lenyu.img.gz
+immortalwrt_sta_uefi=immortalwrt_x86-64-${sta_version}_uefi-gpt_sta_Lenyu.img.gz
 cd bin/targets/x86/64
-md5sum $openwrt_sta > openwrt_sta.md5
-md5sum $openwrt_sta_uefi > openwrt_sta_uefi.md5
+md5sum $immortalwrt_sta > immortalwrt_sta.md5
+md5sum $immortalwrt_sta_uefi > immortalwrt_sta_uefi.md5
 exit 0
 EOF
 
@@ -81,22 +64,22 @@ echo $lenyu_version >  wget/DISTRIB_REVISION1
 echo $lenyu_version | cut -d _ -f 1 >  files/etc/lenyu_version  
 #######
 new_DISTRIB_REVISION=`cat  wget/DISTRIB_REVISION1`
-grep "DISTRIB_REVISION="  package/lean/default-settings/files/zzz-default-settings | cut -d \' -f 2 >  wget/DISTRIB_REVISION3
+grep "DISTRIB_REVISION="  package/emortal/default-settings/files/99-default-settings | cut -d \' -f 2 >  wget/DISTRIB_REVISION3
 old_DISTRIB_REVISION=`cat  wget/DISTRIB_REVISION3`
-sed -i "s/${old_DISTRIB_REVISION}/${new_DISTRIB_REVISION}/"   package/lean/default-settings/files/zzz-default-settings
+sed -i "s/${old_DISTRIB_REVISION}/${new_DISTRIB_REVISION}/"   package/emortal/default-settings/files/99-default-settings
 #
-grep "Check_Update.sh"  package/lean/default-settings/files/zzz-default-settings
+grep "Check_Update.sh"  package/emortal/default-settings/files/99-default-settings
 if [ $? != 0 ]; then
-	sed -i 's/exit 0/ /'  package/lean/default-settings/files/zzz-default-settings
-	cat>> package/lean/default-settings/files/zzz-default-settings<<-EOF
+	sed -i 's/exit 0/ /'  package/emortal/default-settings/files/99-default-settings
+	cat>> package/emortal/default-settings/files/99-default-settings<<-EOF
 	sed -i '$ a alias lenyu="bash /usr/share/Check_Update.sh"' /etc/profile
 	exit 0
 	EOF
 fi
-grep "Lenyu-auto.sh"  package/lean/default-settings/files/zzz-default-settings
+grep "Lenyu-auto.sh"  package/emortal/default-settings/files/99-default-settings
 if [ $? != 0 ]; then
-	sed -i 's/exit 0/ /'  package/lean/default-settings/files/zzz-default-settings
-	cat>> package/lean/default-settings/files/zzz-default-settings<<-EOF
+	sed -i 's/exit 0/ /'  package/emortal/default-settings/files/99-default-settings
+	cat>> package/emortal/default-settings/files/99-default-settings<<-EOF
 	sed -i '$ a alias lenyu-auto="bash /usr/share/Lenyu-auto.sh"' /etc/profile
 	exit 0
 	EOF
@@ -119,22 +102,22 @@ fi
 rm -f /tmp/cloud_version
 # 获取固件云端版本号、内核版本号信息
 current_version=`cat /etc/lenyu_version`
-wget -qO- -t1 -T2 "https://api.github.com/repos/Blueplanet20120/Actions-OpenWrt-x86/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g;s/v//g'  > /tmp/cloud_ts_version
+wget -qO- -t1 -T2 "https://api.github.com/repos/Blueplanet20120/immortalwrt-86/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g;s/v//g'  > /tmp/cloud_ts_version
 if [ -s  "/tmp/cloud_ts_version" ]; then
 	cloud_version=`cat /tmp/cloud_ts_version | cut -d _ -f 1`
 	cloud_kernel=`cat /tmp/cloud_ts_version | cut -d _ -f 2`
 	#固件下载地址
 	new_version=`cat /tmp/cloud_ts_version`
-	DEV_URL=https://github.com/Blueplanet20120/Actions-OpenWrt-x86/releases/download/${new_version}/openwrt_x86-64-${new_version}_sta_Lenyu.img.gz
-	DEV_UEFI_URL=https://github.com/Blueplanet20120/Actions-OpenWrt-x86/releases/download/${new_version}/openwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img.gz
-	openwrt_sta=https://github.com/Blueplanet20120/Actions-OpenWrt-x86/releases/download/${new_version}/openwrt_sta.md5
-	openwrt_sta_uefi=https://github.com/Blueplanet20120/Actions-OpenWrt-x86/releases/download/${new_version}/openwrt_sta_uefi.md5
+	DEV_URL=https://github.com/Blueplanet20120/immortalwrt-86/releases/download/${new_version}/immortalwrt_x86-64-${new_version}_sta_Lenyu.img.gz
+	DEV_UEFI_URL=https://github.com/Blueplanet20120/immortalwrt-86/releases/download/${new_version}/immortalwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img.gz
+	immortalwrt_sta=https://github.com/Blueplanet20120/immortalwrt-86/releases/download/${new_version}/immortalwrt_sta.md5
+	immortalwrt_sta_uefi=https://github.com/Blueplanet20120immortalwrt-86/releases/download/${new_version}/immortalwrt_sta_uefi.md5
 else
 	echo "请检测网络或重试！"
 	exit 1
 fi
 ####
-Firmware_Type="$(grep 'DISTRIB_ARCH=' /etc/openwrt_release | cut -d \' -f 2)"
+Firmware_Type="$(grep 'DISTRIB_ARCH=' /etc/immortalwrt_release | cut -d \' -f 2)"
 echo $Firmware_Type > /etc/lenyu_firmware_type
 echo
 if [[ "$cloud_kernel" =~ "4.19" ]]; then
@@ -146,9 +129,9 @@ fi
 #md5值验证，固件类型判断
 if [ ! -d /sys/firmware/efi ];then
 	if [ "$current_version" != "$cloud_version" ];then
-		wget -P /tmp "$DEV_URL" -O /tmp/openwrt_x86-64-${new_version}_sta_Lenyu.img.gz
-		wget -P /tmp "$openwrt_sta" -O /tmp/openwrt_sta.md5
-		cd /tmp && md5sum -c openwrt_sta.md5
+		wget -P /tmp "$DEV_URL" -O /tmp/immortalwrt_x86-64-${new_version}_sta_Lenyu.img.gz
+		wget -P /tmp "$immortalwrt_sta" -O /tmp/immortalwrt_sta.md5
+		cd /tmp && md5sum -c immortalwrt_sta.md5
 		if [ $? != 0 ]; then
       echo "您下载文件失败，请检查网络重试…"
       sleep 4
@@ -162,9 +145,9 @@ if [ ! -d /sys/firmware/efi ];then
 	fi
 else
 	if [ "$current_version" != "$cloud_version" ];then
-		wget -P /tmp "$DEV_UEFI_URL" -O /tmp/openwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img.gz
-		wget -P /tmp "$openwrt_sta_uefi" -O /tmp/openwrt_sta_uefi.md5
-		cd /tmp && md5sum -c openwrt_sta_uefi.md5
+		wget -P /tmp "$DEV_UEFI_URL" -O /tmp/immortalwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img.gz
+		wget -P /tmp "$immortalwrt_sta_uefi" -O /tmp/immortalwrt_sta_uefi.md5
+		cd /tmp && md5sum -c immortalwrt_sta_uefi.md5
 		if [ $? != 0 ]; then
       echo "您下载文件失败，请检查网络重试…"
       sleep 4
@@ -191,11 +174,11 @@ case $num1 in
 	echo
 	sleep 3
 	if [ ! -d /sys/firmware/efi ];then
-		gzip -d openwrt_x86-64-${new_version}_sta_Lenyu.img.gz
-		sysupgrade /tmp/openwrt_x86-64-${new_version}_sta_Lenyu.img
+		gzip -d immortalwrt_x86-64-${new_version}_sta_Lenyu.img.gz
+		sysupgrade /tmp/immortalwrt_x86-64-${new_version}_sta_Lenyu.img
 	else
-		gzip -d openwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img.gz
-		sysupgrade /tmp/openwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img
+		gzip -d immortalwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img.gz
+		sysupgrade /tmp/immortalwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img
 	fi
     ;;
     n|N)
@@ -204,11 +187,11 @@ case $num1 in
     echo
     sleep 3
 	if [ ! -d /sys/firmware/efi ];then
-		gzip -d openwrt_x86-64-${new_version}_sta_Lenyu.img.gz
-		sysupgrade -n  /tmp/openwrt_x86-64-${new_version}_sta_Lenyu.img
+		gzip -d immortalwrt_x86-64-${new_version}_sta_Lenyu.img.gz
+		sysupgrade -n  /tmp/immortalwrt_x86-64-${new_version}_sta_Lenyu.img
 	else
-		gzip -d openwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img.gz
-		sysupgrade -n  /tmp/openwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img
+		gzip -d immortalwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img.gz
+		sysupgrade -n  /tmp/immortalwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img
 	fi
     ;;
     *)
@@ -251,7 +234,7 @@ EOF
 
 cat>files/usr/share/Lenyu-auto.sh<<-\EOF
 #!/bin/bash
-# https://github.com/Blueplanet20120/Actions-OpenWrt-x86
+# https://github.com/Blueplanet20120/immortalwrt-86
 # Actions-OpenWrt-x86 By Lenyu 20210505
 #path=$(dirname $(readlink -f $0))
 # cd ${path}
@@ -265,22 +248,22 @@ fi
 rm -f /tmp/cloud_version
 # 获取固件云端版本号、内核版本号信息
 current_version=`cat /etc/lenyu_version`
-wget -qO- -t1 -T2 "https://api.github.com/repos/Blueplanet20120/Actions-OpenWrt-x86/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g;s/v//g'  > /tmp/cloud_ts_version
+wget -qO- -t1 -T2 "https://api.github.com/repos/Blueplanet20120/immortalwrt-86/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g;s/v//g'  > /tmp/cloud_ts_version
 if [ -s  "/tmp/cloud_ts_version" ]; then
 	cloud_version=`cat /tmp/cloud_ts_version | cut -d _ -f 1`
 	cloud_kernel=`cat /tmp/cloud_ts_version | cut -d _ -f 2`
 	#固件下载地址
 	new_version=`cat /tmp/cloud_ts_version`
-	DEV_URL=https://github.com/Blueplanet20120/Actions-OpenWrt-x86/releases/download/${new_version}/openwrt_x86-64-${new_version}_sta_Lenyu.img.gz
-	DEV_UEFI_URL=https://github.com/Blueplanet20120/Actions-OpenWrt-x86/releases/download/${new_version}/openwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img.gz
-	openwrt_sta=https://github.com/Blueplanet20120/Actions-OpenWrt-x86/releases/download/${new_version}/openwrt_sta.md5
-	openwrt_sta_uefi=https://github.com/Blueplanet20120/Actions-OpenWrt-x86/releases/download/${new_version}/openwrt_sta_uefi.md5
+	DEV_URL=https://github.com/Blueplanet20120/immortalwrt-86/releases/download/${new_version}/immortalwrt_x86-64-${new_version}_sta_Lenyu.img.gz
+	DEV_UEFI_URL=https://github.com/Blueplanet20120/immortalwrt-86/releases/download/${new_version}/immortalwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img.gz
+	immortalwrt_sta=https://github.com/Blueplanet20120/immortalwrt-86/releases/download/${new_version}/immortalwrt_sta.md5
+	immortalwrt_sta_uefi=https://github.com/Blueplanet20120/immortalwrt-86/releases/download/${new_version}/immortalwrt_sta_uefi.md5
 else
 	echo "请检测网络或重试！"
 	exit 1
 fi
 ####
-Firmware_Type="$(grep 'DISTRIB_ARCH=' /etc/openwrt_release | cut -d \' -f 2)"
+Firmware_Type="$(grep 'DISTRIB_ARCH=' /etc/immortalwrt_release | cut -d \' -f 2)"
 echo $Firmware_Type > /etc/lenyu_firmware_type
 echo
 if [[ "$cloud_kernel" =~ "4.19" ]]; then
@@ -292,16 +275,16 @@ fi
 #md5值验证，固件类型判断
 if [ ! -d /sys/firmware/efi ];then
 	if [ "$current_version" != "$cloud_version" ];then
-		wget -P /tmp "$DEV_URL" -O /tmp/openwrt_x86-64-${new_version}_sta_Lenyu.img.gz
-		wget -P /tmp "$openwrt_sta" -O /tmp/openwrt_sta.md5
-		cd /tmp && md5sum -c openwrt_sta.md5
+		wget -P /tmp "$DEV_URL" -O /tmp/immortalwrt_x86-64-${new_version}_sta_Lenyu.img.gz
+		wget -P /tmp "$immortalwrt_sta" -O /tmp/immortalwrt_sta.md5
+		cd /tmp && md5sum -c immortalwrt_sta.md5
 		if [ $? != 0 ]; then
 		  echo "您下载文件失败，请检查网络重试…"
 		  sleep 4
 		  exit
 		fi
-		gzip -d /tmp/openwrt_x86-64-${new_version}_sta_Lenyu.img.gz
-		sysupgrade /tmp/openwrt_x86-64-${new_version}_sta_Lenyu.img
+		gzip -d /tmp/immortalwrt_x86-64-${new_version}_sta_Lenyu.img.gz
+		sysupgrade /tmp/immortalwrt_x86-64-${new_version}_sta_Lenyu.img
 	else
 		echo -e "\033[32m 本地已经是最新版本，还更个鸡巴毛啊… \033[0m"
 		echo
@@ -309,16 +292,16 @@ if [ ! -d /sys/firmware/efi ];then
 	fi
 else
 	if [ "$current_version" != "$cloud_version" ];then
-		wget -P /tmp "$DEV_UEFI_URL" -O /tmp/openwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img.gz
-		wget -P /tmp "$openwrt_sta_uefi" -O /tmp/openwrt_sta_uefi.md5
-		cd /tmp && md5sum -c openwrt_sta_uefi.md5
+		wget -P /tmp "$DEV_UEFI_URL" -O /tmp/immortalwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img.gz
+		wget -P /tmp "$immortalwrt_sta_uefi" -O /tmp/immortalwrt_sta_uefi.md5
+		cd /tmp && md5sum -c immortalwrt_sta_uefi.md5
 		if [ $? != 0 ]; then
 			echo "您下载文件失败，请检查网络重试…"
 			sleep 1
 			exit
 		fi
-		gzip -d /tmp/openwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img.gz
-		sysupgrade /tmp/openwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img
+		gzip -d /tmp/immortalwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img.gz
+		sysupgrade /tmp/immortalwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img
 	else
 		echo -e "\033[32m 本地已经是最新版本，还更个鸡巴毛啊… \033[0m"
 		echo
