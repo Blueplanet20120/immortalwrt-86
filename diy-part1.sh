a609566f3f15b5c094f369fefc728e910fa32a66
Blue planet <30463977+Blueplanet20120@users.noreply.github.com>
2022-08-05 23:46:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -102,7 +102,8 @@ fi
 rm -f /tmp/cloud_version
 # 获取固件云端版本号、内核版本号信息
 current_version=`cat /etc/lenyu_version`
-wget -qO- -t1 -T2 "https://api.github.com/repos/Blueplanet20120/immortalwrt-86/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g;s/v//g'  > /tmp/cloud_ts_version
+curl -s https://api.github.com/repos/Blueplanet20120/immortalwrt-86/releases/latest | grep 'tag_name' | cut -d\" -f4 > /tmp/cloud_ts_version
+sleep 3
 if [ -s  "/tmp/cloud_ts_version" ]; then
 	cloud_version=`cat /tmp/cloud_ts_version | cut -d _ -f 1`
 	cloud_kernel=`cat /tmp/cloud_ts_version | cut -d _ -f 2`
@@ -240,73 +241,76 @@ cat>files/usr/share/Lenyu-auto.sh<<-\EOF
 # cd ${path}
 #检测准备
 if [ ! -f  "/etc/lenyu_version" ]; then
-	echo
-	echo -e "\033[31m 该脚本在非Lenyu固件上运行，为避免不必要的麻烦，准备退出… \033[0m"
-	echo
-	exit 0
+echo
+echo -e "\033[31m 该脚本在非Lenyu固件上运行，为避免不必要的麻烦，准备退出… \033[0m"
+echo
+exit 0
 fi
 rm -f /tmp/cloud_version
 # 获取固件云端版本号、内核版本号信息
 current_version=`cat /etc/lenyu_version`
-wget -qO- -t1 -T2 "https://api.github.com/repos/Blueplanet20120/immortalwrt-86/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g;s/v//g'  > /tmp/cloud_ts_version
+# wget -qO- -T2 "https://api.github.com/repos/Blueplanet20120/immortalwrt-86/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g;s/v//g'  > /tmp/cloud_ts_version
+# 因immortalwrt不支持上述格式.
+curl -s https://api.github.com/repos/Blueplanet20120/immortalwrt-86/releases/latest | grep 'tag_name' | cut -d\" -f4 > /tmp/cloud_ts_version
+sleep 3
 if [ -s  "/tmp/cloud_ts_version" ]; then
-	cloud_version=`cat /tmp/cloud_ts_version | cut -d _ -f 1`
-	cloud_kernel=`cat /tmp/cloud_ts_version | cut -d _ -f 2`
-	#固件下载地址
-	new_version=`cat /tmp/cloud_ts_version`
-	DEV_URL=https://github.com/Blueplanet20120/immortalwrt-86/releases/download/${new_version}/immortalwrt_x86-64-${new_version}_sta_Lenyu.img.gz
-	DEV_UEFI_URL=https://github.com/Blueplanet20120/immortalwrt-86/releases/download/${new_version}/immortalwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img.gz
-	immortalwrt_sta=https://github.com/Blueplanet20120/immortalwrt-86/releases/download/${new_version}/immortalwrt_sta.md5
-	immortalwrt_sta_uefi=https://github.com/Blueplanet20120/immortalwrt-86/releases/download/${new_version}/immortalwrt_sta_uefi.md5
+cloud_version=`cat /tmp/cloud_ts_version | cut -d _ -f 1`
+cloud_kernel=`cat /tmp/cloud_ts_version | cut -d _ -f 2`
+#固件下载地址
+new_version=`cat /tmp/cloud_ts_version` # 2208052057_5.4.203
+DEV_URL=https://github.com/Blueplanet20120/immortalwrt-86/releases/download/${new_version}/immortalwrt_x86-64-${new_version}_sta_Lenyu.img.gz
+DEV_UEFI_URL=https://github.com/Blueplanet20120/immortalwrt-86/releases/download/${new_version}/immortalwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img.gz
+immortalwrt_sta=https://github.com/Blueplanet20120/immortalwrt-86/releases/download/${new_version}/immortalwrt_sta.md5
+immortalwrt_sta_uefi=https://github.com/Blueplanet20120/immortalwrt-86/releases/download/${new_version}/immortalwrt_sta_uefi.md5
 else
-	echo "请检测网络或重试！"
-	exit 1
+echo "请检测网络或重试！"
+exit 1
 fi
 ####
-Firmware_Type="$(grep 'DISTRIB_ARCH=' /etc/immortalwrt_release | cut -d \' -f 2)"
+Firmware_Type="$(grep 'DISTRIB_ARCH=' /etc/lenyu_version | cut -d \' -f 2)"
 echo $Firmware_Type > /etc/lenyu_firmware_type
 echo
 if [[ "$cloud_kernel" =~ "4.19" ]]; then
-	echo
-	echo -e "\033[31m 该脚本在Lenyu固件Sta版本上运行，目前只建议在Dev版本上运行，准备退出… \033[0m"
-	echo
-	exit 0
+echo
+echo -e "\033[31m 该脚本在Lenyu固件Sta版本上运行，目前只建议在Dev版本上运行，准备退出… \033[0m"
+echo
+exit 0
 fi
 #md5值验证，固件类型判断
 if [ ! -d /sys/firmware/efi ];then
-	if [ "$current_version" != "$cloud_version" ];then
-		wget -P /tmp "$DEV_URL" -O /tmp/immortalwrt_x86-64-${new_version}_sta_Lenyu.img.gz
-		wget -P /tmp "$immortalwrt_sta" -O /tmp/immortalwrt_sta.md5
-		cd /tmp && md5sum -c immortalwrt_sta.md5
-		if [ $? != 0 ]; then
-		  echo "您下载文件失败，请检查网络重试…"
-		  sleep 4
-		  exit
-		fi
-		gzip -d /tmp/immortalwrt_x86-64-${new_version}_sta_Lenyu.img.gz
-		sysupgrade /tmp/immortalwrt_x86-64-${new_version}_sta_Lenyu.img
-	else
-		echo -e "\033[32m 本地已经是最新版本，还更个鸡巴毛啊… \033[0m"
-		echo
-		exit
-	fi
+if [ "$current_version" != "$cloud_version" ];then
+wget -P /tmp "$DEV_URL" -O /tmp/immortalwrt_x86-64-${new_version}_sta_Lenyu.img.gz
+wget -P /tmp "$immortalwrt_sta" -O /tmp/immortalwrt_sta.md5
+cd /tmp && md5sum -c immortalwrt_sta.md5
+if [ $? != 0 ]; then
+  echo "您下载文件失败，请检查网络重试…"
+  sleep 4
+  exit
+fi
+gzip -d /tmp/immortalwrt_x86-64-${new_version}_sta_Lenyu.img.gz
+sysupgrade /tmp/immortalwrt_x86-64-${new_version}_sta_Lenyu.img
 else
-	if [ "$current_version" != "$cloud_version" ];then
-		wget -P /tmp "$DEV_UEFI_URL" -O /tmp/immortalwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img.gz
-		wget -P /tmp "$immortalwrt_sta_uefi" -O /tmp/immortalwrt_sta_uefi.md5
-		cd /tmp && md5sum -c immortalwrt_sta_uefi.md5
-		if [ $? != 0 ]; then
-			echo "您下载文件失败，请检查网络重试…"
-			sleep 1
-			exit
-		fi
-		gzip -d /tmp/immortalwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img.gz
-		sysupgrade /tmp/immortalwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img
-	else
-		echo -e "\033[32m 本地已经是最新版本，还更个鸡巴毛啊… \033[0m"
-		echo
-		exit
-	fi
+echo -e "\033[32m 本地已经是最新版本，还更个鸡巴毛啊… \033[0m"
+echo
+exit
+fi
+else
+if [ "$current_version" != "$cloud_version" ];then
+wget -P /tmp "$DEV_UEFI_URL" -O /tmp/immortalwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img.gz
+wget -P /tmp "$immortalwrt_sta_uefi" -O /tmp/immortalwrt_sta_uefi.md5
+cd /tmp && md5sum -c immortalwrt_sta_uefi.md5
+if [ $? != 0 ]; then
+echo "您下载文件失败，请检查网络重试…"
+sleep 1
+exit
+fi
+gzip -d /tmp/immortalwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img.gz
+sysupgrade /tmp/immortalwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img
+else
+echo -e "\033[32m 本地已经是最新版本，还更个鸡巴毛啊… \033[0m"
+echo
+exit
+fi
 fi
 
 exit 0
