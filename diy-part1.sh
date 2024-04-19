75d37b146e15b930da0b5201f2acf04f6f38ba6c
Blue planet <30463977+Blueplanet20120@users.noreply.github.com>
2024-04-20 06:17:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -114,20 +114,18 @@ grep "xray_backup"  package/emortal/default-settings/files/99-default-settings
 if [ $? != 0 ]; then
 	sed -i 's/exit 0/ /'  package/emortal/default-settings/files/99-default-settings
 	cat>> package/emortal/default-settings/files/99-default-settings<<-EOF
-		cat << 'EOFF' > /etc/rc.local #用单引号 'EOFF' 来包裹文本内容，内容就不会被 shell 解释保持原样写入。
-		# Put your custom commands here that should be executed once
-		# the system init finished. By default this file does nothing.
-		if [ -f "/etc/xray_backup/xray_backup" ]; then
-		cp -f /etc/xray_backup/xray_backup /usr/bin/xray
-		# chmod +x /usr/bin/xray
-		# Check if the copy operation was successful
-		  if [ $? -eq 0 ]; then
-		 touch /tmp/xray_succ.log
-		  fi
-		rm -rf  /etc/xray_backup/xray_backup
-		fi
-		exit 0
-		EOFF
+	echo '# Put your custom commands here that should be executed once
+	# the system init finished. By default this file does nothing.
+	if [ -f "/etc/xray_backup/xray_backup" ]; then
+	cp -f /etc/xray_backup/xray_backup /usr/bin/xray
+	# chmod +x /usr/bin/xray
+	# Check if the copy operation was successful
+	  if [ $? -eq 0 ]; then
+	 touch /tmp/xray_succ.log
+	  fi
+	rm -rf  /etc/xray_backup/xray_backup
+	fi
+	exit 0' > /etc/rc.local
 	exit 0
 	EOF
 fi
