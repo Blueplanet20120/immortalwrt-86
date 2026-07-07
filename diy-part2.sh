474f118dabf7704cf7b033eef4d303f94850a6cf
Blue planet <30463977+Blueplanet20120@users.noreply.github.com>
2026-07-07 20:08:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,8 +16,4 @@ sed -i '/^VERSION_NUMBER:=$(if/ s/24\.10-SNAPSHOT/24.10-lenyu/' include/version.
 #sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.10/g' target/linux/x86/Makefile
 #sed -i "s/.*PKG_VERSION:=.*/PKG_VERSION:=4.3.9_v1.2.14/" package/lean/qBittorrent-static/Makefile
 #sed -i 's/download-ci-llvm = true/download-ci-llvm = false/g' feeds/packages/lang/rust/Makefile
-# 移除自带的低版本 golang
-rm -rf feeds/packages/lang/golang
-# 指定拉取支持 1.25 版本的开源 golang 仓库分支
-git clone https://github.com/kenzok8/golang -b 1.25 feeds/packages/lang/golang
 # welcome test 
